b1d4568cd8181f1866de02f5c97fa47d7e053c64
djagatiya <divyeshjagatiya@gmail.com>
2022-08-01 21:10:46 +0530

    v8 added.

---
+++ models/v8_efficientnet_b0.py
@@ -0,0 +1,30 @@
+import timm
+
+import torch
+import torch.nn as nn
+from torchvision import transforms, models
+
+
+class MyModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = timm.create_model("efficientnet_b0", num_classes=0, pretrained=True)
+        self.avg_pooler = nn.AdaptiveAvgPool1d(64)
+
+    def forward(self, x):
+        x = transforms.functional.resize(x,size=[256, 256])
+        x = transforms.functional.center_crop(x, 224)
+        
+        x = x/255.0
+        x = transforms.functional.normalize(x, 
+                                                mean=[0.485, 0.456, 0.406], 
+                                                std=[0.229, 0.224, 0.225])
+        x = self.feature_extractor(x)
+        x = self.avg_pooler(x)
+        return x
+
+
+model = MyModel()
+model.eval()
+pass
