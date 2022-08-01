2166e5ebfd4b0a3507bb34d4c0dd237589aa3bae
djagatiya <divyeshjagatiya@gmail.com>
2022-08-01 20:57:51 +0530

    v7 added.

---
+++ models/v7_swinv2_base_window16_256.py
@@ -0,0 +1,28 @@
+import timm
+
+import torch
+import torch.nn as nn
+from torchvision import transforms, models
+
+
+class MyModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = timm.create_model("swinv2_base_window16_256", num_classes=0, pretrained=True)
+        self.avg_pooler = nn.AdaptiveAvgPool1d(64)
+
+    def forward(self, x):
+        x = transforms.functional.resize(x,size=[256, 256])
+        x = x/255.0
+        x = transforms.functional.normalize(x, 
+                                                mean=[0.485, 0.456, 0.406], 
+                                                std=[0.229, 0.224, 0.225])
+        x = self.feature_extractor(x)
+        x = self.avg_pooler(x)
+        return x
+
+
+model = MyModel()
+model.eval()
+pass
