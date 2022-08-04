87b179219e260a32e648d8ced0133f75e7fbfea2
djagatiya <divyeshjagatiya@gmail.com>
2022-08-04 10:31:53 +0530

    v11 added.

---
+++ models/v11_swin_base_patch4_window7_224_in22k.py
@@ -0,0 +1,60 @@
+# Run
+# 32.3s - GPU
+
+# Public Score
+# 0.350
+
+import timm
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import torchvision.transforms.functional as F
+
+
+class MyModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.base_model = timm.create_model("swin_base_patch4_window7_224_in22k", pretrained=True)
+        self.feature_extractor = torch.nn.Sequential(*list(self.base_model.children())[:-1])
+        self.avg_pooler = nn.AdaptiveAvgPool1d(64)
+        self.expacted = 256
+
+    def zero_padding(self, img):
+        h,w = img.shape[-2:]
+        # print(h, w)
+
+        h_pad = self.expacted - h
+        if h_pad < 0: h_pad = 0
+
+        w_pad = self.expacted - w
+        if w_pad < 0: w_pad = 0
+
+        if w_pad == 0 and h_pad == 0:
+            return img
+
+        img = F.pad(img, [w_pad, h_pad])
+
+        return img
+
+
+    def forward(self, x):
+        x = self.zero_padding(x)
+        x = transforms.functional.resize(x,size=[256, 256])
+        x = transforms.functional.center_crop(x, 224)
+
+        x = x/255.0
+        x = transforms.functional.normalize(x, 
+                                                mean=[0.485, 0.456, 0.406], 
+                                                std=[0.229, 0.224, 0.225])
+        x = self.feature_extractor(x)
+        x = x.mean(dim=1)
+        x = self.avg_pooler(x)
+        
+        return x
+
+
+model = MyModel()
+model.eval()
+pass
